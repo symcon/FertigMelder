a2ba09d0ec9737e213a36279db291581172f6df5
Till Brede <tb@symcon.de>
2019-09-27 16:42:55 +0200

    added validation test for FertigMelder (PHP-66)

---
+++ tests/FertigMelderValidationTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+include_once __DIR__ . '/stubs/Validator.php';
+class FertigMelderValidationTest extends TestCaseSymconValidation
+{
+    public function testValidateFertigMelder(): void
+    {
+        $this->validateLibrary(__DIR__ . '/..');
+    }
+    public function testValidateFertigMelderModule(): void
+    {
+        $this->validateModule(__DIR__ . '/../FertigMelder');
+    }
+}
